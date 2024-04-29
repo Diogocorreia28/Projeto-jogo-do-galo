6bb8b07a507675982306bd240f8cc3ab5582a6a3
Diogocorreia28 <166063539+Diogocorreia28@users.noreply.github.com>
2024-04-29 21:09:37 +0100

    Update Funcoes.h

--- Funcoes.h
+++ Funcoes.h
@@ -15,30 +15,19 @@ protected:
     int opçao_dificuldade;
 
 
-     
-
 public:
     //usadas no main
     int opçaomenu_jogo,i_jogo=0,dificuldade_main;
     int coluna_main,linha_main;
     int j_main,r_main=1;
     string confirmar_main;
-    string resultado_main;
     char x_main=' ',y=' ',z=' ',w=' ',s=' ',d=' ',a=' ',b=' ',c=' ';
     char matriz_de_jogo[3][3] = {{x_main,y,z},
                                  {a,b,c},
                                  {w,d,s}
     };
 
-    //usadas no top10
-    string linha;
-    const int Maxjogos=10;
-    int numeroJogos=0;
 
-    struct Jogo {
-    string resultado;
-     
-    }jogo[10];
    
     Funcoes();
     ~Funcoes();
@@ -73,7 +62,22 @@ public:
 
     void jogada_nivel3(char matriz_de_jogo[3][3],int &linha,int &coluna);
 
-    //top 10
+};
+
+class top10   : public Funcoes{
+   
+    public:
+    string linha;
+    const int Maxjogos=10;
+    int numeroJogos=0;
+    string resultado_main;
+
+    struct Jogo {
+    string resultado;
+     
+    }jogo[10];
+
+        //top 10
     void salvarJogos(const Jogo jogos[], int numeroJogos);
 
     void carregarJogos(Jogo jogos[], int& numeroJogos);
@@ -82,5 +86,8 @@ public:
 
     void mostrarTop10(const Jogo jogos[], int numeroJogos);
 };
+
+
+
 
 #endif
